cddf142359f3694c33d6a8e5270de1e377f521b7
Raziel619 <raziel619dev@gmail.com>
2022-04-16 16:13:31 -0400

    Add pokedex package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/pokedex.dart';
 
 void main() {
   runApp(const MyApp());
@@ -58,6 +59,16 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    print("hello");
+    final dex = Pokedex();
+    dex.pokemon.get(name: 'aegislash-blade').then((aegislashSpecies) {
+      print(aegislashSpecies);
     });
   }
 
