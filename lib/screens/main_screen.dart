560fe9a665453830482814bc6d7d357ce24ae057
Govinda Hosein <gh604721@digicelgroup.com>
2022-04-18 23:11:55 -0400

    Add flutter_keyboard_visibility

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:whose_that_pkmn/constants/app_arrays.dart';
 
 import '../constants/asset_paths.dart';
@@ -15,48 +16,67 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(AssetPaths.IMG_BG_MAIN), fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.white.withOpacity(0.2),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 50, bottom: 30),
-                child: Text(
-                  "Who's That\nPkmn?!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(height: 1.5, fontSize: 22),
-                ),
-              ),
-              Text("1/3", style: TextStyle(height: 1.5, fontSize: 18)),
-              Container(
-                margin: EdgeInsets.all(16),
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
-                    )),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.matrix(AppArrays.BW_FILTER),
-                  child: FadeInImage.memoryNetwork(
-                    width: (MediaQuery.of(context).size.width * 0.7),
-                    placeholder: Uint8List(2),
-                    fit: BoxFit.fitHeight,
-                    image:
-                        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
+    return KeyboardVisibilityBuilder(
+      builder: (context, isKeyboardVisible) {
+        return Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(AssetPaths.IMG_BG_MAIN),
+                  fit: BoxFit.cover)),
+          child: Scaffold(
+            backgroundColor: Colors.white.withOpacity(0.2),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  isKeyboardVisible
+                      ? SizedBox.shrink()
+                      : Padding(
+                          padding: EdgeInsets.only(top: 50, bottom: 30),
+                          child: Text(
+                            "Who's That\nPkmn?!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 1.5, fontSize: 22),
+                          ),
+                        ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Text("1/3",
+                        style: TextStyle(height: 1.5, fontSize: 18)),
                   ),
-                ),
-              )
-            ],
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          )),
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.matrix(AppArrays.BW_FILTER),
+                        child: FadeInImage.memoryNetwork(
+                          width: (MediaQuery.of(context).size.width * 0.6),
+                          placeholder: Uint8List(2),
+                          fit: BoxFit.fitHeight,
+                          image:
+                              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a search term',
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
