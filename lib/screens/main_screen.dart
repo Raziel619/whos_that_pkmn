4e050cc1ec46352f46d7f39e4e42ab0fbb5854be
Govinda Hosein <gh604721@digicelgroup.com>
2022-04-21 01:51:38 -0400

    Fixed overflow on main_screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
 
+import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:whose_that_pkmn/constants/app_arrays.dart';
 
 import '../constants/asset_paths.dart';
@@ -13,14 +13,16 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
   String _image =
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png";
   ColorFilter _bw_filter = ColorFilter.matrix(AppArrays.BW_FILTER);
+  bool _isKeyboardOpen = false;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance?.addObserver(this);
     Future.delayed(const Duration(milliseconds: 10000), () {
       setState(() {
         // _image =
@@ -34,67 +36,75 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    final value = MediaQuery.of(context).viewInsets.bottom;
+    setState(() {
+      _isKeyboardOpen = value > 0;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return KeyboardVisibilityBuilder(
-      builder: (context, isKeyboardVisible) {
-        return Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(AssetPaths.IMG_BG_MAIN),
-                  fit: BoxFit.cover)),
-          child: Scaffold(
-            backgroundColor: Colors.white.withOpacity(0.2),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  isKeyboardVisible
-                      ? SizedBox.shrink()
-                      : Padding(
-                          padding: EdgeInsets.only(top: 50, bottom: 30),
-                          child: Text(
-                            "Who's That\nPkmn?!",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(height: 1.5, fontSize: 22),
-                          ),
-                        ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Text("1/3",
-                        style: TextStyle(height: 1.5, fontSize: 18)),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(16),
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20),
-                          )),
-                      child: ColorFiltered(
-                        colorFilter: _bw_filter,
-                        child: FadeInImage.memoryNetwork(
-                          width: (MediaQuery.of(context).size.width * 0.6),
-                          placeholder: Uint8List(2),
-                          fit: BoxFit.fitHeight,
-                          image: _image,
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(AssetPaths.IMG_BG_MAIN), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.white.withOpacity(0.2),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              _isKeyboardOpen
+                  ? SizedBox.shrink()
+                  : DelayedDisplay(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 50, bottom: 30),
+                        child: Text(
+                          "Who's That\nPkmn?!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(height: 1.5, fontSize: 22),
                         ),
                       ),
                     ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter a search term',
-                    ),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text("1/3", style: TextStyle(height: 1.5, fontSize: 18)),
               ),
-            ),
+              Container(
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    )),
+                child: ColorFiltered(
+                  colorFilter: _bw_filter,
+                  child: FadeInImage.memoryNetwork(
+                    width: (MediaQuery.of(context).size.width * 0.6),
+                    placeholder: Uint8List(256),
+                    fit: BoxFit.fitHeight,
+                    image: _image,
+                  ),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a search term',
+                ),
+              ),
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
