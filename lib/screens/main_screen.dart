1227bff4a0a889f1cdff178b7bc84bbb2affeea1
Govinda Hosein <gh604721@digicelgroup.com>
2022-05-02 15:03:26 -0400

    Refactor main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -23,45 +23,59 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(AssetPaths.IMG_BG_MAIN), fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.white.withOpacity(0.2),
-        body: Consumer<PokeProvider>(builder: (context, pokeProvider, child) {
-          final key = UniqueKey();
-          switch (_selectedIndex) {
-            case 0:
-              return pokeProvider.isTodayQuizzesComplete()
-                  ? Text("Quizzes complete")
-                  : PlayPartial(
-                      pokeProvider,
-                      key: key,
-                    );
-            case 1:
-              return Text("Leaderboard");
-            default:
-              return Text("History");
-          }
-        }),
-        bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: _selectedIndex == 0
-              ? AppColors.GREY_1.withOpacity(0.85)
-              : Colors.transparent,
-          color: AppColors.PRIMARY_PINK_COMP,
-          items: const <Widget>[
-            Icon(Icons.question_mark_rounded, size: 30),
-            Icon(Icons.leaderboard_rounded, size: 30),
-            Icon(Icons.list_alt_rounded, size: 30)
-          ],
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
+    return Consumer<PokeProvider>(builder: (context, pokeProvider, child) {
+      return Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(AssetPaths.IMG_BG_MAIN), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.white.withOpacity(0.2),
+          body: _determinePartial(pokeProvider),
+          bottomNavigationBar: CurvedNavigationBar(
+            backgroundColor: _determineBottomNavColor(pokeProvider),
+            color: AppColors.PRIMARY_PINK_COMP,
+            items: const <Widget>[
+              Icon(Icons.question_mark_rounded, size: 30),
+              Icon(Icons.leaderboard_rounded, size: 30),
+              Icon(Icons.list_alt_rounded, size: 30)
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
         ),
-      ),
-    );
+      );
+    });
+
   }
+  
+  Widget _determinePartial(PokeProvider pokeProvider){
+    final key = UniqueKey();
+    switch (_selectedIndex) {
+      case 0:
+        return pokeProvider.isTodayQuizzesComplete()
+            ? Text("Quizzes complete")
+            : PlayPartial(
+          pokeProvider,
+          key: key,
+        );
+      case 1:
+        return Text("Leaderboard");
+      default:
+        return Text("History");
+    }
+  }
+  
+  Color _determineBottomNavColor(PokeProvider pokeProvider) {
+    Color col = Colors.transparent;
+    if (_selectedIndex == 0) {
+      if (!pokeProvider.isTodayQuizzesComplete()) {
+        col = AppColors.GREY_1.withOpacity(0.85);
+      }
+    }
+    return col;
+  }
+
 }
