62bb4f4deb205f95ed584f280bfffc05ce3b1cec
Govinda Hosein <gh604721@digicelgroup.com>
2022-05-01 20:04:41 -0400

    Add bottom nav screens

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:whose_that_pkmn/constants/app_arrays.dart';
 import 'package:whose_that_pkmn/providers/poke_provider.dart';
+import 'package:whose_that_pkmn/screens/partials/play_partial.dart';
 
 import '../constants/app_colors.dart';
 import '../constants/asset_paths.dart';
@@ -18,40 +19,12 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
-  String _image = "";
-  ColorFilter _bw_filter = ColorFilter.matrix(AppArrays.BW_FILTER);
-  bool _isKeyboardOpen = false;
-  late PokeProvider _pokeProvider;
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _pokeProvider = Provider.of<PokeProvider>(context, listen: false);
-    _image = _pokeProvider.todayQuizzes[0].sprite_url;
-    WidgetsBinding.instance?.addObserver(this);
-    Future.delayed(const Duration(milliseconds: 10000), () {
-      setState(() {
-        _bw_filter = ColorFilter.mode(
-          Colors.transparent,
-          BlendMode.multiply,
-        );
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance?.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeMetrics() {
-    final value = MediaQuery.of(context).viewInsets.bottom;
-    setState(() {
-      _isKeyboardOpen = value > 0;
-    });
   }
 
   @override
@@ -62,54 +35,16 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
               image: AssetImage(AssetPaths.IMG_BG_MAIN), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.white.withOpacity(0.2),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              _isKeyboardOpen
-                  ? SizedBox.shrink()
-                  : DelayedDisplay(
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 50, bottom: 30),
-                        child: Text(
-                          "Who's That\nPkmn?!",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(height: 1.5, fontSize: 22),
-                        ),
-                      ),
-                    ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text("1/3", style: TextStyle(height: 1.5, fontSize: 18)),
-              ),
-              Container(
-                margin: EdgeInsets.all(16),
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
-                    )),
-                child: ColorFiltered(
-                  colorFilter: _bw_filter,
-                  child: FadeInImage.memoryNetwork(
-                    width: (MediaQuery.of(context).size.width * 0.6),
-                    placeholder: _pokeProvider.pokeballIcon,
-                    placeholderScale: 0.1,
-                    fit: BoxFit.fitHeight,
-                    image: _image,
-                  ),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a search term',
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: Consumer<PokeProvider>(builder: (context, pokeProvider, child) {
+          switch (_selectedIndex) {
+            case 0:
+              return PlayPartial(pokeProvider);
+            case 1:
+              return Text("Leaderboard");
+            default:
+              return Text("History");
+          }
+        }),
         bottomNavigationBar: CurvedNavigationBar(
           backgroundColor: Colors.transparent,
           color: AppColors.PRIMAR_PINK_COMP,
@@ -119,7 +54,9 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
             Icon(Icons.list_alt_rounded, size: 30)
           ],
           onTap: (index) {
-            print("tapped");
+            setState(() {
+              _selectedIndex = index;
+            });
           },
         ),
       ),
---
+++ lib/screens/partials/play_partial.dart
@@ -0,0 +1,102 @@
+import 'package:delayed_display/delayed_display.dart';
+import 'package:flutter/material.dart';
+import 'package:whose_that_pkmn/providers/poke_provider.dart';
+import '../../constants/app_arrays.dart';
+
+class PlayPartial extends StatefulWidget {
+  PokeProvider pokeProvider;
+
+  PlayPartial(this.pokeProvider, {Key? key}) : super(key: key);
+
+  @override
+  State<PlayPartial> createState() => _PlayPartialState();
+}
+
+class _PlayPartialState extends State<PlayPartial> with WidgetsBindingObserver {
+  String _image = "";
+  ColorFilter _bw_filter = ColorFilter.matrix(AppArrays.BW_FILTER);
+  bool _isKeyboardOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _image = widget.pokeProvider.todayQuizzes[0].sprite_url;
+    WidgetsBinding.instance?.addObserver(this);
+    // Future.delayed(const Duration(milliseconds: 10000), () {
+    //   setState(() {
+    //     _bw_filter = ColorFilter.mode(
+    //       Colors.transparent,
+    //       BlendMode.multiply,
+    //     );
+    //   });
+    // });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    if (!mounted) return;
+
+    final value = MediaQuery.of(context).viewInsets.bottom;
+    setState(() {
+      _isKeyboardOpen = value > 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          _isKeyboardOpen
+              ? SizedBox.shrink()
+              : DelayedDisplay(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 50, bottom: 30),
+                    child: Text(
+                      "Who's That\nPkmn?!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 1.5, fontSize: 22),
+                    ),
+                  ),
+                ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Text("1/3", style: TextStyle(height: 1.5, fontSize: 18)),
+          ),
+          Container(
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                )),
+            child: ColorFiltered(
+              colorFilter: _bw_filter,
+              child: FadeInImage.memoryNetwork(
+                width: (MediaQuery.of(context).size.width * 0.6),
+                placeholder: widget.pokeProvider.pokeballIcon,
+                placeholderScale: 0.1,
+                fit: BoxFit.fitHeight,
+                image: _image,
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
