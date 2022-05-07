7b0c5fd6cb269e28cf93eadba9e75b4752f43cbc
Govinda Hosein <govinda.hosein@digicelgroup.com>
2022-05-07 19:22:24 -0400

    Add interstitial ad to AdProvider

--- lib/providers/ad_provider.dart
+++ lib/providers/ad_provider.dart
@@ -2,27 +2,35 @@ import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class BannerAdWithFlag {
-  bool loaded;
-  BannerAd ad;
-
-  BannerAdWithFlag(this.loaded, this.ad);
-}
-
 class AdProvider with ChangeNotifier {
   static const _androidAppID = "ca-app-pub-7786369819729095~5789120325";
   static const _iosAppID = "ca-app-pub-7786369819729095~4831261879";
-  static const _androidInterstitialID = "ca-app-pub-7786369819729095/1849875317";
+  static const _androidInterstitialID =
+      "ca-app-pub-7786369819729095/1849875317";
   static const _iosInterstitialID = "ca-app-pub-7786369819729095/3518180204";
 
+  InterstitialAd? _interstitialAd;
   bool _isInitialized = false;
+
   bool get isInitialized => _isInitialized;
+
+  InterstitialAd? get interstitialAd => _interstitialAd;
 
   Future<void> initialize() async {
     print("Initializing AdProvider");
+    await InterstitialAd.load(
+        adUnitId:
+            Platform.isAndroid ? _androidInterstitialID : _iosInterstitialID,
+        request: AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            _interstitialAd = ad;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error');
+          },
+        ));
     _isInitialized = true;
     notifyListeners();
   }
-
-
 }
